5371451cd9bc0c6e56ff1d58e9428d27a42cb18b
hertzr <45162795+hertzr@users.noreply.github.com>
2020-03-17 08:32:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
